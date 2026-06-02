3859194aeda64d9455ff1df42109f0a146155926
Balaram Rajendran <balaram.rajendran@arm.com>
2026-06-02 20:04:02 +0530

    Initial Code for Spacelift-iac-orchestration

--- main.tf
+++ main.tf
@@ -1,17 +1,31 @@
 terraform {
-    required_providers {
-        aws = {
-        source = "hashicorp/aws"
-        version = "~> 4.16"
-}
-required_version = ">= 1.2.0"
-provider "aws" {
-region = "us-east-1"
-}
-resource "aws_instance" "spacelift_app_server" {
-    ami = "ami-02396cdd13e9a1257"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "app-server"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.31.0.0/16"
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0685bcc683dadb6b9"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
 }
