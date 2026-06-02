e287020a3d4c2b626cb31d64f2cff64a3ebd2b6f
Balaram Rajendran <balaram.rajendran@arm.com>
2026-06-02 20:34:50 +0530

    Add default values for instance_name and vpc_name variables

--- variables.tf
+++ variables.tf
@@ -1,12 +1,11 @@
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
-
+  default     = "spacelift-app-server"
 }
 
 variable "vpc_name" {
-  description = "name of vpc"
+  description = "Name of the VPC"
   type        = string
+  default     = "spacelift-vpc"
 }
-
-# random variables
