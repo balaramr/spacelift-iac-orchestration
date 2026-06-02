e38e01eff07b0f8021382d44c0f17cd5210710cd
Balaram Rajendran <balaram.rajendran@arm.com>
2026-06-02 22:41:01 +0530

    Initial Code for Spacelift-iac-orchestration

--- main.tf
+++ main.tf
@@ -7,8 +7,19 @@ terraform {
   }
 }
 
+# 1. Configured Region to eu-west-1
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-1"
+}
+
+# 2. Configured clean Ubuntu 22.04 LTS AMI for eu-west-1
+resource "aws_instance" "app_server" {
+  ami           = "ami-0d940f23d527c3041"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Spacelift-Managed-Server"
+  }
 }
 
 data "aws_caller_identity" "current" {}
