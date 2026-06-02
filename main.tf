7f9df2768ac4efb2f3070a28fb0914803c9ef25b
Balaram Rajendran <balaram.rajendran@arm.com>
2026-06-02 23:13:27 +0530

    Updated Code for Spacelift-iac-orchestration

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "172.31.16.0/16"
   tags = {
     name    = "Orbit Labs VPC"
     project = "Orbit-labs"
@@ -21,7 +21,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "172.31.16.0/20"
   availability_zone = "eu-west-1a"
   tags = {
     name    = "Orbit Labs Subnet"
@@ -34,4 +34,4 @@ output "subnet_id" {
   description = "ID of the main subnet"
 }
 
-# 
+#
