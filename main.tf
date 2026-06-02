b02ddbb0aab61c1b163801afb666b5a298706964
Balaram Rajendran <balaram.rajendran@arm.com>
2026-06-02 23:02:41 +0530

    Updated Code for Spacelift-iac-orchestration

--- main.tf
+++ main.tf
@@ -34,13 +34,12 @@ output "aws_account_id" {
 resource "aws_s3_bucket" "orbit_storage" {
   bucket_prefix = "orbit-storage-"
 
-  tags = {
-    name        = "Orbit Labs Storage"
-    managedBy   = "Spacelift"
-    mission     = "First Launch"
-    project     = "Orbit-labs"
-    environment = "demo"
-  }
+tags = {
+  name        = "Orbit Labs Storage"
+  managedBy   = "Spacelift"
+  mission     = "First Launch"
+  environment = "demo"
+}
 }
 
 
