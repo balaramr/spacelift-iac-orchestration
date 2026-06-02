e474fb69210af668fc429bfbde7d1101f8f234fc
Balaram Rajendran <balaram.rajendran@arm.com>
2026-06-02 23:08:00 +0530

    Updated Code for Spacelift-iac-orchestration

--- main.tf
+++ main.tf
@@ -11,36 +11,25 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-# Dynamically fetch the latest stable Ubuntu 22.04 AMI for eu-west-1
-data "aws_ssm_parameter" "ubuntu_ami" {
-  name = "/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = data.aws_ssm_parameter.ubuntu_ami.value # Uses the dynamic ID found above
-  instance_type = "t2.micro"
-
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "Spacelift-Managed-Server"
+    name    = "Orbit Labs VPC"
+    project = "Orbit-labs"
   }
 }
 
-data "aws_caller_identity" "current" {}
-
-output "aws_account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
-
-resource "aws_s3_bucket" "orbit_storage" {
-  bucket_prefix = "orbit-storage-"
-
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-west-1a"
   tags = {
-    name        = "Orbit Labs Storage"
-    managedBy   = "Spacelift"
-    mission     = "First Launch"
-    project     = "Orbit-labs"
-    environment = "demo"
+    name    = "Orbit Labs Subnet"
+    project = "Orbit-labs"
   }
 }
 
-
+output "subnet_id" {
+  value       = aws_subnet.main.id
+  description = "ID of the main subnet"
+}
